bf6f6544ae4e521f5648469179535c784274f48b
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-26 21:18:26 -0500

    Complete Circle

---
+++ Workshop/GeometricFigure/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    public class Circle : GeometricFigure
+    {
+        private double _r;
+        public Circle(string name, double r) : base(name)
+        {
+            R = r;
+        }
+
+        public double R 
+        { 
+            get => _r; 
+            set => _r = ValidateR(value); 
+        }
+
+        public override double GetArea() => Math.PI * R * R;
+
+
+        public override double GetPerimeter() => 2 * Math.PI * R;
+
+        private double ValidateR(double r)
+        {
+            if (r <= 0)
+            {
+                throw new ArgumentException("Radius must be greater than zero.");
+            }
+            return r;
+        }
+    }
+}
--- Workshop/GeometricFigure/GeometricFigure.cs
+++ Workshop/GeometricFigure/GeometricFigure.cs
@@ -1,7 +1,21 @@
 ﻿namespace Geometry
 {
-    public abstract class GeometricFigure;
-    
+    public abstract class GeometricFigure
+    {
 
-    
+        protected GeometricFigure(string name) 
+        {
+            Name = name;
+        }
+        public string Name { get; set; } = null!;
+
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+        public override string ToString()
+        {
+            return $"{Name}: Area = {GetArea():F5} Perimeter = {GetPerimeter():F5}";
+        }
+  
+    }
 }
